ec2b085111c6ccb26434e4a1a8f3c55b2541ee15
ArefinAnwar <70402736+ArefinAnwar@users.noreply.github.com>
2022-02-25 12:38:56 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA3PrU8WIBYlNcTgbAVZdcRH0HuEzXfMJA",
-  authDomain: "learning-97e21.firebaseapp.com",
-  databaseURL: "https://learning-97e21-default-rtdb.firebaseio.com",
-  projectId: "learning-97e21",
-  storageBucket: "learning-97e21.appspot.com",
-  messagingSenderId: "15948519487",
-  appId: "1:15948519487:web:20aa66b15058febef4008a",
-  measurementId: "G-SJHWDTV8LZ",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
